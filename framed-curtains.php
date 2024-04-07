c5c7aac6fe605d80a3bf3a9d0ae4e410b3e46d7c
Jack36VRN <36456429+Jack36VRN@users.noreply.github.com>
2024-04-07 11:17:15 +0300

    Update 05.04.2024
    Скрыт текст, главный офис

--- framed-curtains.php
+++ framed-curtains.php
@@ -250,6 +250,22 @@ Template Name: Шаблон страницы Шторы в рамке
         </div>
         </div>
     </div>
+    
+
+    <div id="contacts" class="block">
+        <div class="contacts curve shadow txt-white">
+          <h2>Контакты</h2>
+          <div>
+            <!-- <p class="lh-1-5">Главный офис</p> //в городах, где нет представительства будем указывать пункты выдачи -->
+            <p class="adress"><a href="https://yandex.ru/maps/-/CDF1BJY1" target="_blank">ул. Димитрова, д. 112<br>г. Воронеж<br>394028</a></p>
+          </div>
+        </div>
+        <div id="map-container" class="map shadow">
+          <script type="text/javascript" charset="utf-8" async src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3A8ae9a1c4cc9b4fa79d12b3501ec530e120cbaab8475765ff7967f55f97adb624&amp;width=100%25&amp;height=360&amp;lang=ru_RU&amp;scroll=true"></script>
+
+        </div>
+
+    </div>
 </main>
 
 <?php get_footer(); ?>
